a3b9553f267594a5e5c9def42f95390a0d1d8d1a
Ondrej Kupka <ondra.cap@gmail.com>
2014-02-24 01:38:16 +0100

    wip

--- gerrit/events.go
+++ gerrit/events.go
@@ -19,41 +19,75 @@ type EventStream struct {
 }
 
 type PatchsetCreatedEvent struct {
-
+	Type     string
+	Change   *Change
+	PatchSet *PatchSet
+	Uploader *Account
 }
 
 type DraftPublishedEvent struct {
-
+	Type     string
+	Change   *Change
+	PatchSet *PatchSet
+	Uploader *Account
 }
 
 type ChangeAbandonedEvent struct {
-
+	Type      string
+	Change    *Change
+	PatchSet  *PatchSet
+	Abandoner *Account
+	Reason    string
 }
 
 type ChangeRestoredEvent struct {
-
+	Type     string
+	Change   *Change
+	PatchSet *PatchSet
+	Restorer *Account
+	Reason   string
 }
 
 type ChangeMergedEvent struct {
-
+	Type      string
+	Change    *Change
+	PatchSet  *PatchSet
+	Submitter *Account
 }
 
 type MergeFailedEvent struct {
-
+	Type      string
+	Change    *Change
+	PatchSet  *PatchSet
+	Submitter *Account
+	Reason    string
 }
 
 type CommentAddedEvent struct {
-
+	Type      string
+	Change    *Change
+	PatchSet  *PatchSet
+	Author    *Account
+	Approvals []Approval
+	Comment   string
 }
 
 type RefUpdatedEvent struct {
-
+	Type      string
+	Submitter *Account
+	RefUpdate *RefUpdate
 }
 
 type ReviewerAddedEvent struct {
-
+	Type     string
+	Change   *Change
+	PatchSet *PatchSet
+	Reviewer *Account
 }
 
 type TopicChangedEvent struct {
-
+	Type     string
+	Change   *Change
+	Changer  *Account
+	OldTopic string
 }
